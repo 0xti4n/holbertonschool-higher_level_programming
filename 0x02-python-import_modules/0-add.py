dc1169421c6a1b7a52eb0933ddf601c27a66893a
cristian gomez <1154@holbertonschool.com>
2019-12-12 17:39:54 +0000

    Module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
 if __name__ == "__main__":
-    a, b = 1, 2
-    result = add(a, b)
-    print('{:d} + {:d} = {:d}'.format(a, b, result))
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
