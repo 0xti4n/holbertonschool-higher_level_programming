9fa68d31302fa0861a459f27780eae7cc4f03ada
Cristian Gomez <1154@holbertonschool.com>
2019-12-18 05:15:58 +0000

    Add tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if not tuple_a and not tuple_b:
+        return (0, 0)
+    if not tuple_a:
+        return tuple_b
+    if not tuple_b:
+        return (tuple_a)
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        a = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+        return a
+    if (len(tuple_a) < 2 and len(tuple_b) < 2):
+        a = (tuple_a[0] + tuple_b[0], 0)
+        return a
+    if len(tuple_a) < 2:
+        a = tuple_a[0] + tuple_b[0], tuple_b[1]
+        return a
+    if len(tuple_b) < 2:
+        a = tuple_a[0] + tuple_b[0], tuple_a[1]
+        return a
