3a576a858d46ab2c05ce3f8811dc15e901bdd15f
Cristian Gomez <1154@holbertonschool.com>
2019-12-20 02:57:09 +0000

    Roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,9 +7,15 @@ def roman_to_int(roman_string):
         r = 0
         if len(roman_string) < 2:
             r = a[roman_string[0]]
+        elif len(roman_string) == 2:
+            if a[roman_string[-2]] < a[roman_string[-1]]:
+                r = a[roman_string[-1]] - a[roman_string[-2]]
         else:
+            st = roman_string
             for i in roman_string:
                 r += sum(map(lambda x: a[x] if x == i else 0, a))
-            if a[roman_string[-2]] < a[roman_string[-1]]:
-                r -= a[roman_string[-2]] * 2
-        return r
+            d = range(len(st) - 1, 0, -1)
+            f = sum(map(lambda x: a[st[x - 1]] if a[st[x]] > a[st[x - 1]]
+                    else 0, d))
+            r -= 2 * f
+    return r
