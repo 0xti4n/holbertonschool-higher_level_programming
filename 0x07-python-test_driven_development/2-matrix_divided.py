5cdc794feac9f69b61cb463fcaffb6f4c182dcca
Cristian gomez <1154@holbertonschool.com>
2020-01-12 10:37:56 -0500

    Fix error message

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,16 +9,16 @@ def matrix_divided(matrix, div):
 
     for li in matrix:
         if type(li) is not list:
-            raise TypeError('matrix must be a matrix\
-                (list of lists) of integers/floats')
+            raise TypeError('matrix must be a matrix \
+(list of lists) of integers/floats')
         if len(li) != len(matrix[0]):
             raise TypeError('Each row of the matrix must have the same size')
 
     for i in range(len(matrix) - 1):
             for j in range(len(matrix[i])):
                 if type(matrix[i][j]) is not int and float:
-                    raise TypeError('matrix must be a matrix\
-                        (list of lists) of integers/floats')
+                    raise TypeError('matrix must be a matrix \
+(list of lists) of integers/floats')
 
     return list(map(lambda i: list(map
                     (lambda j: round(j / div, 2), i)), matrix))
