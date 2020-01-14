e1f81a971d10b141842d9b4b4eb5300d1db8036f
Cristian gomez <1154@holbertonschool.com>
2020-01-13 20:02:23 -0500

    Test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,20 +5,25 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    
-    def test_nomal_list(self):
-        li = [1, 2, 3, 4]
-        self.assertEqual(max_integer(li), 4)
 
-    def test_with_string(self):
+    def test_max(self):
+        self.assertTrue(max_integer([1, 2, 3, 2233]), 2233)
+
+    def test_empty(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_max_int(self):
+        self.assertEqual(max_integer([1, 3, 333333333]), 333333333)
+
+    def test_float(self):
+        self.assertNotEqual(max_integer([4.2, 3, 5]), 4.2)
+
+    def test_error(self):
         with self.assertRaises(TypeError):
-            li = [1, 3, "hola", 4]
-            max_integer(li)		           
+            self.assertRaises(max_integer(10))
 
-    def test_with_negative(self):
-        li = [1, 2, 3, -4]
-        self.assertEqual(max_integer(li), 3)
+    def test_neg(self):
+        self.assertNotEqual(max_integer([2, 3, -5]), -5)
 
-    def empty_list(self):
-        li = []
-        self.assertIsNone(max_integer(li))
+    def test_is_not_max(self):
+        self.assertIsNot(max_integer([1, 4, 5]), 4)
