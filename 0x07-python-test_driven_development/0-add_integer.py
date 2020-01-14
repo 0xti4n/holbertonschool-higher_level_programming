99064b1a441bd42e6be61598a8374be08cf036ef
Cristian Gomez <1154@holbertonschool.com>
2020-01-14 12:35:06 +0000

    add condition if a:

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,13 +10,12 @@ def add_integer(a, b=98):
     """Function to function that adds 2 integers
     If the num is none, the function returns error
     """
-    if a != a:
-        a = 89
-    if b != b:
-        b = 89
-    if a is None or (type(a) is not int and type(a) is not float):
-        raise TypeError('a must be an integer')
-    if type(b) is not int and type(b) is not float:
-        raise TypeError('b must be an integer')
+    if a:
+        if a is None or (type(a) is not int and type(a) is not float):
+            raise TypeError('a must be an integer')
+        if type(b) is not int and type(b) is not float:
+            raise TypeError('b must be an integer')
+        else:
+            return int(a) + int(b)
     else:
-        return int(a) + int(b)
+        raise TypeError('a must be an integer')
