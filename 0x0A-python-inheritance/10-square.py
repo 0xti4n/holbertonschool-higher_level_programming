8f1f4ddfc0e5621e890c6385d6e3eb1f1777ab85
Cristian Gomez <1154@holbertonschool.com>
2020-01-21 06:08:52 +0000

    hotfix quit area

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,6 +6,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Funcion"""
     def __init__(self, size):
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
