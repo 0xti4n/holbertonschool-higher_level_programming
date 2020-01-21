c89a2c946a7fe2c589257d419397e7681400b58c
Cristian Gomez <1154@holbertonschool.com>
2020-01-21 05:28:39 +0000

    change integer validator

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,7 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """class Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
-        super().integer_validator("height", height)
         super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
