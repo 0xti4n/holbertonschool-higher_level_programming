08f80451276affb02f248063366dbb51b38aa458
cristian gomez <1154@holbertonschool.com>
2020-01-21 22:49:35 +0000

    add task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student by"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        d = {}
+        if type(attrs) is list:
+            for i in attrs:
+                if type(i) is str:
+                    try:
+                        d[i] = getattr(self, i)
+                    except AttributeError:
+                        continue
+                return d
+        else:
+            return self.__dict__
