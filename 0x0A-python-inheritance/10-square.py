9fef892ad2b9af87adc5115bf0fe664b6aa82a9d
Cristian Gomez <1154@holbertonschool.com>
2020-01-21 06:06:19 +0000

    hotfix quit area

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,6 +9,3 @@ class Square(Rectangle):
         super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        return self.__size ** 2
