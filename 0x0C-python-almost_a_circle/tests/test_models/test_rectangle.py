91bc2c2c0a304ab6ba516cc80b3cddc6c6c9427d
Cristian Gomez <1154@holbertonschool.com>
2020-01-28 02:41:23 +0000

    fix all tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Test Rectangle"""
 import unittest
 import pep8
 from models.base import Base
@@ -6,10 +7,9 @@ from models.rectangle import Rectangle
 
 
 class Testrectangle(unittest.TestCase):
-    """ """
+    """ test """
     def test_pep8_conformance_rectangle(self):
-        """Test that we conform to PEP8
-        """
+        """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/rectangle.py'])
         self.assertEqual(result.total_errors, 0, "Found code style errors \
@@ -19,16 +19,16 @@ class Testrectangle(unittest.TestCase):
         self.assertTrue(issubclass(Rectangle, Base))
 
     def test_parameters(self):
-        """ """
+        """test"""
         r1 = Rectangle(10, 2)
         r2 = Rectangle(2, 10)
         r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.id, 4)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r1.height, 2)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
-        self.assertEqual(r2.id, 2)
+        self.assertEqual(r2.id, 5)
         self.assertEqual(r2.width, 2)
         self.assertEqual(r2.height, 10)
         self.assertEqual(r2.x, 0)
@@ -39,17 +39,15 @@ class Testrectangle(unittest.TestCase):
         self.assertEqual(r3.x, 0)
         self.assertEqual(r3.y, 0)
         with self.assertRaises(TypeError):
-                r4 = Rectangle()
-    """Task 3 """
-    def is_string(self):
+            r4 = Rectangle()
+
+    def test_string(self):
         with self.assertRaises(TypeError):
             R1 = Rectangle(1.01, 3)
         with self.assertRaises(TypeError):
             R1 = Rectangle(1.01, 3)
 
     def test_type_param(self):
-        """Task 3 """
-        """ WIDTH TESTING """
         with self.assertRaises(TypeError):
             R1 = Rectangle(1.01, 3)
             raise TypeError()
@@ -62,7 +60,6 @@ class Testrectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             R4 = Rectangle(True, 4)
             raise TypeError()
-        """ HEIGHT TESTING """
         with self.assertRaises(TypeError):
             H1 = Rectangle(5, 1.76)
             raise TypeError()
@@ -74,8 +71,7 @@ class Testrectangle(unittest.TestCase):
             raise TypeError()
         with self.assertRaises(ValueError):
             H1 = Rectangle(5, -4798576398576)
-            raise ValueError()
-        """ X TESTING """
+            raise ValueError
         with self.assertRaises(TypeError):
             H1 = Rectangle(5, 1, 1.50)
             raise TypeError()
@@ -88,7 +84,6 @@ class Testrectangle(unittest.TestCase):
         with self.assertRaises(ValueError):
             H1 = Rectangle(5, 7, -4798576398576)
             raise ValueError()
-        """ Y TESTING """
         with self.assertRaises(TypeError):
             H1 = Rectangle(5, 1, 1, 1.53)
             raise TypeError()
@@ -101,6 +96,3 @@ class Testrectangle(unittest.TestCase):
         with self.assertRaises(ValueError):
             H1 = Rectangle(5, 9, 5, -4798576398576)
             raise ValueError()
-
-if __name__ == "_main_":
-    unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Test Square"""
 import unittest
 import pep8
 from models.base import Base
@@ -7,15 +7,15 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 
-class TestSquare(unittest.TestCase):
-
+class Testsquare(unittest.TestCase):
+    """ """
     def test_pep8_conformance_square(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/square.py'])
         self.assertEqual(result.total_errors, 0, "Found code style errors \
             (and warnings).")
-    """Task 11"""
+
     def test_getter(self):
         r1 = Square(5)
         self.assertEqual(r1.size, 5)
@@ -75,7 +75,22 @@ class TestSquare(unittest.TestCase):
         r1.size = 6
         self.assertEqual(r1.width, 6)
         self.assertEqual(r1.height, 6)
-    """end task 11"""
 
-if __name__ == "_main_":
-    unittest.main()
+    def test_to_dictionary(self):
+
+        Base._Base__nb_objects = 0
+
+        s1 = Square(10, 2, 1, 9)
+        s1_dictionary = s1.to_dictionary()
+        expected = {'id': 9, 'x': 2, 'size': 10, 'y': 1}
+        self.assertEqual(s1_dictionary, expected)
+
+        s1 = Square(1, 0, 0, 9)
+        s1_dictionary = s1.to_dictionary()
+        expected = {'id': 9, 'x': 0, 'size': 1, 'y': 0}
+        self.assertEqual(s1_dictionary, expected)
+
+        s1.update(5, 5, 5, 5)
+        s1_dictionary = s1.to_dictionary()
+        expected = {'id': 5, 'x': 5, 'size': 5, 'y': 5}
+        self.assertEqual(s1_dictionary, expected)
