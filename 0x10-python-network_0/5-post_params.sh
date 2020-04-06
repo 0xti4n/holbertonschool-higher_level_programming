79b2ca8d581d2156500cdf1efb097cc9523deb39
Cristian <1154@holbertonschool.com>
2020-04-06 19:18:18 +0000

    Add -s in file

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # task 5
-curl -d 'email=hr@holbertonschool.com' -d 'subject=I will always be here for PLD' "$1"
+curl -s -d 'email=hr@holbertonschool.com' -d 'subject=I will always be here for PLD' "$1"
