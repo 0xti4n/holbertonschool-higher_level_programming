dd4fcf7f23265a72104e10fa8e4636cfac7543c9
Cristian <1154@holbertonschool.com>
2020-04-11 14:41:19 +0000

    Updating peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,4 +5,5 @@
 def find_peak(list_of_integers):
     """Function that find a peak"""
     if list_of_integers:
-        return max(list_of_integers)
+        ls = sorted(list_of_integers)
+        return ls[-1]
