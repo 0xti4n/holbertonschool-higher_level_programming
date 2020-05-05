e3e64ab19d33d6ac1fb9719d215f551d67eac63a
Cristian <1154@holbertonschool.com>
2020-05-05 19:13:30 +0000

    Updating task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,9 +4,9 @@ const process = require('process');
 const args = process.argv;
 args.splice(0, 2);
 
-if (args.length === 0 || args.length === 1){
+if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
   args.sort((a, b) => b - a);
   console.log(parseInt(args[1]));
-  }
+}
