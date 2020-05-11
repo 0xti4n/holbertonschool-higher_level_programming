172b3964db9f231072051cfa7a9c1b2ea45b9ad4
Cristian <1154@holbertonschool.com>
2020-05-11 21:58:47 +0000

    Adding task 102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const fs = require('fs');
+fs.readFile(args[0], 'utf-8', (err, fileA) => {
+  if (err) console.log(err);
+  fs.readFile(args[1], 'utf-8', (err, fileB) => {
+    if (err) console.log(err);
+    fs.writeFile(args[2], fileA + fileB, (err) => {
+      if (err) console.log(err);
+    });
+  });
+});
