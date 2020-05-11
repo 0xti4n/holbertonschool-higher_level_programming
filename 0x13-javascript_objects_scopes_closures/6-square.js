43354f0c26f797dffee22022604308f8c882be56
Cristian <1154@holbertonschool.com>
2020-05-11 17:11:38 +0000

    Updating task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const Sq = require('./5-square');
 
 module.exports = class Square extends Sq {
   charPrint (c) {
-    if (!c) {
+    if (c === undefined) {
       this.print();
     } else {
       const letter = 'C';
