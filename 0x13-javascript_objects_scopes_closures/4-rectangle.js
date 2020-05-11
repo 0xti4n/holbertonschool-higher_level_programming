9d991b75ad5d238c8b082990a91cb51fed643e19
Cristian <1154@holbertonschool.com>
2020-05-11 14:07:00 +0000

    Adding task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w && h) {
+      if ((w !== 0 && w > 0) && (h !== 0 && h > 0)) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  }
+
+  print () {
+    const letter = 'X';
+    for (let col = 0; col < this.height; col++) {
+      console.log(letter.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
