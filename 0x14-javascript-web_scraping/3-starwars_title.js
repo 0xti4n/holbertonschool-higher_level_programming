f13b4b91904042d8f80302c5e1149a23f49becdf
Cristian <1154@holbertonschool.com>
2020-05-12 17:55:00 +0000

    Updating task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,14 +3,14 @@
 const args = process.argv.slice(2);
 const request = require('request');
 
-if (args.length !== 0) {
-  const url = 'https://swapi-api.hbtn.io/api/films/' + args[0];
-  request.get(url, (error, response) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const data = JSON.parse(response.body);
-      console.log(data.title);
-    }
-  });
-}
+const url = `https://swapi-api.hbtn.io/api/films/${args[0]}`;
+request.get(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 404) {
+    console.log(response.statusCode);
+  } else {
+    const data = JSON.parse(response.body);
+    console.log(data.title);
+  }
+});
