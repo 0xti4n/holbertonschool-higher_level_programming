6b39fb563e4c242fda736ff206916dcb794e84bc
Cristian <1154@holbertonschool.com>
2020-05-12 17:16:19 +0000

    Updating task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,10 @@
 const args = process.argv.slice(2);
 const fs = require('fs');
 
-fs.readFile(args[0], 'utf-8', (err, file) => {
-  if (err) console.log(err);
-  console.log(file);
+fs.readFile(args[0], 'utf-8', (error, file) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(file);
+  }
 });
